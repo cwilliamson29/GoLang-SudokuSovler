fb8f7d11cabe2df5b3871072175569560f7626d3
Chris Williamson <chriswill0629@gmail.com>
2023-05-29 22:59:39 -0400

    finished puzzle solver

--- main.go
+++ main.go
@@ -29,14 +29,58 @@ func getEmptySpace(puzz [][]int) (int, int) {
 				return i, j
 			}
 		}
-		fmt.Println()
 	}
 	return -1, -1
 }
 
+func isNumValid(puzz [][]int, guess int, row int, column int) bool {
+	for index := range puzz {
+		if puzz[row][index] == guess && column != index {
+			return false
+		}
+	}
+
+	for index := range puzz {
+		if puzz[index][column] == guess && row != index {
+			return false
+		}
+	}
+
+	// Is number valid for box
+	// row 0 & column 3
+	for k := 0; k < 3; k++ {
+		for l := 0; l < 3; l++ {
+			if puzz[row-row%3+k][column-column%3+k] == guess && (row-row%3+k != row || column-column%3+l != column) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func solvePuzzle(puzz [][]int) bool {
+	row, column := getEmptySpace(puzz)
+	if row == -1 {
+		return true
+	} else {
+		row, column = getEmptySpace(puzz)
+	}
+	for i := 1; i <= 9; i++ {
+		if isNumValid(puzz, i, row, column) {
+			puzz[row][column] = i
+
+			if solvePuzzle(puzz) {
+				return true
+			}
+			puzz[row][column] = 0
+		}
+	}
+	return false
+}
+
 func main() {
 	puzz := [][]int{
-		{1, 0, 0, 0, 3, 5, 0, 7, 0},
+		{0, 0, 0, 0, 3, 5, 0, 7, 0},
 		{2, 5, 0, 0, 4, 6, 8, 0, 1},
 		{0, 1, 3, 7, 0, 8, 0, 4, 9},
 		{1, 9, 0, 0, 0, 7, 0, 0, 4},
@@ -44,7 +88,7 @@ func main() {
 		{8, 0, 2, 0, 9, 4, 0, 0, 7},
 		{3, 7, 0, 0, 0, 9, 0, 0, 0},
 		{0, 6, 1, 0, 7, 0, 0, 0, 0},
-		{4, 0, 0, 5, 8, 1, 0, 0, 1},
+		{4, 0, 0, 5, 8, 1, 0, 0, 0},
 	}
 
 	displayBoard(puzz)
@@ -55,4 +99,11 @@ func main() {
 	} else {
 		fmt.Println("Puzzle is solved")
 	}
+
+	fmt.Println(isNumValid(puzz, 7, 4, 0))
+
+	displayBoard(puzz)
+	fmt.Println()
+	solvePuzzle(puzz)
+	displayBoard(puzz)
 }
